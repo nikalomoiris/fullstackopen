742a2048dd34333dceedeafc2a2fadf90cbea99e
nikalomiris <nikalomiris@gmail.com>
2019-07-06 06:51:54 -0400

    Part 2 phonebook step 4 solution

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -9,13 +9,14 @@ const Numbers = ({ persons }) => {
 const App = () => {
 
   const [ persons, setPersons ] = useState([
-    {
-      name: 'Arto Hellas',
-      phone: '555-555-5555'
-    }
+    { name: 'Arto Hellas', phone: '040-123456' },
+    { name: 'Ada Lovelace', phone: '39-44-5323523' },
+    { name: 'Dan Abramov', phone: '12-43-234345' },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
   ]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
@@ -23,6 +24,10 @@ const App = () => {
 
   const handlePhoneChange = (event) => {
     setNewPhone(event.target.value);
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value);
   }
 
   const addNumber = (event) => {
@@ -56,7 +61,11 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+        <div>
+          filter: <input value={filter} onChange={handleFilterChange} />
+        </div>
+      <Numbers persons={persons.filter(person =>
+        person.name.toLowerCase().includes(filter.toLowerCase()))} />
     </div>
   );
 }
